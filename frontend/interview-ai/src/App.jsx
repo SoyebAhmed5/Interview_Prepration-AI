814d36f8d540d29d04b8d9128aab8d33f1848fa6
SoyebAhmed5 <soyebahmed.5632@gmail.com>
2025-09-19 01:10:02 +0530

    Defining App Routes

--- frontend/interview-ai/src/App.jsx
+++ frontend/interview-ai/src/App.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import { Toaster} from "react-hot-toast";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-import Login from "./pages/Auth/Login";
-import SignUp from "./pages/Auth/SignUp";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Home/Dashboard";
-import InterviewPrep from "./pages/InterviewPrep/InterviewPrep"
+import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Router>
+        <Routes>
+          {/*Default Route*/}
+          <Route path="/" element={<LandingPage />} />
+        
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/interview-prep/:sessionId" element={<InterviewPrep />}/>
+        </Routes>
+      </Router>
 
-export default App
+      <Toaster
+        toastOptions={{
+          ClassName: "",
+          style: {
+            frontsize: "13px",
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default App;
