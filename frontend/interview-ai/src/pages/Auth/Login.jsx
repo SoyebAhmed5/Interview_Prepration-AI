64759c440924ba0944a52151e443c8afb3329eee
SoyebAhmed5 <soyebahmed.5632@gmail.com>
2025-10-02 21:07:11 +0530

    Building Login Popup Form UI

--- frontend/interview-ai/src/pages/Auth/Login.jsx
+++ frontend/interview-ai/src/pages/Auth/Login.jsx
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/input";
 
 const Login = ({ setCurrentPage }) => {
-  
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+  //Handle Login form Submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+        Please enter your details to log in
+      </p>
+
+      <form onSubmit={handleLogin}>
+        <Input
+          valve={email}
+          onChange={({ target }) => setEmail(target.value)}
+          label="Email Address"
+          placeholder="john@example.com"
+          type="text"
+        />
+
+        <Input
+          valve={password}
+          onChange={({ target }) => setEmail(target.value)}
+          label="Password"
+          placeholder="Min 8 character"
+          type="password"
+        />
+
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+        <button type="submit" className="btn-primary">
+          LOGIN
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don't have an account?{" "}
+            <button
+             className="font-medium text-primary underline cursor-pointer"
+            onClick={() => {
+              setCurrentPage("signup");
+            }}>
+              SignUp
+              </button>
+          </p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
